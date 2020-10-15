46639cf39f5232a35124200bb23c0ee62c491369
4thel00z <4thel00z@gmail.com>
2020-10-16 01:29:29 +0200

    [master] Refactored debug module; added new Request model
    - renamed longPath to LongPath to make it accessible for
    validator logic
    - moved debug.GenericResponse to global models.go
    - Added new Request model for validation test

--- pkg/libemail/debug/models.go
+++ pkg/libemail/debug/models.go
@@ -2,12 +2,11 @@ package debug
 
 import "emaild/pkg/libemail"
 
-type GenericResponse struct {
-	Message interface{} `json:"message"`
-	Error   *string     `json:"error,omitempty"`
-}
-
 type GetRoutesResponse struct {
 	Routes []libemail.Route `json:"routes"`
 	Error  *string          `json:"error,omitempty"`
+}
+
+type PostValidatorTestRequest struct {
+	HasToBeThere string `json:"has_to_be_there" validate:"empty=false"`
 }
